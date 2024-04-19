abaa08e2dc3344e48c1950660f781831dbb6e647
sahalali <sahal@culturecreates.com>
2024-04-19 16:42:28 +0530

    Test GET recon

--- src/controller/recon/recon.controller.ts
+++ src/controller/recon/recon.controller.ts
@@ -10,30 +10,30 @@ export class ReconciliationController {
   constructor(private readonly _reconciliationService: ReconciliationService) {
   }
 
-  @Get("/recon")
-  @ApiOperation({ summary: "Reconcile" })
-  @ApiResponse({ status: 200, type: ReconciliationResponse, isArray: true, description: "Reconciliation response" })
-  @ApiResponse({ status: 500, description: "Internal server error" })
-  @ApiQuery({
-    name: "name",
-    description: "Name of the entity to reconcile",
-    required: true,
-    explode: false,
-    type: String
-  })
-  @ApiQuery({
-    name: "type",
-    description: "Type of the entity to reconcile",
-    required: true,
-    explode: false,
-    enum: Object.values(ReconciliationTypesEnum)
-  })
-  async reconcileByQuery(
-    @Query("name") name: string,
-    @Query("type") type: string
-  ): Promise<ReconciliationResponse[]> {
-    return await this._reconciliationService.reconcileByQuery(name, type);
-  }
+  // @Get("/recon")
+  // @ApiOperation({ summary: "Reconcile" })
+  // @ApiResponse({ status: 200, type: ReconciliationResponse, isArray: true, description: "Reconciliation response" })
+  // @ApiResponse({ status: 500, description: "Internal server error" })
+  // @ApiQuery({
+  //   name: "name",
+  //   description: "Name of the entity to reconcile",
+  //   required: true,
+  //   explode: false,
+  //   type: String
+  // })
+  // @ApiQuery({
+  //   name: "type",
+  //   description: "Type of the entity to reconcile",
+  //   required: true,
+  //   explode: false,
+  //   enum: Object.values(ReconciliationTypesEnum)
+  // })
+  // async reconcileByQuery(
+  //   @Query("name") name: string,
+  //   @Query("type") type: string
+  // ): Promise<ReconciliationResponse[]> {
+  //   return await this._reconciliationService.reconcileByQuery(name, type);
+  // }
 
   @Post("/recon")
   @ApiOperation({ summary: "Reconcile" })
