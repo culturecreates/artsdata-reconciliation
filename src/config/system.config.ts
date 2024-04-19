cfa8cde2aa6e9f0f2b9d4c1755859688106fc1b7
sahalali <sahal@culturecreates.com>
2024-04-19 13:15:01 +0530

    Added https certificate

--- src/config/system.config.ts
+++ src/config/system.config.ts
@@ -6,3 +6,8 @@ const { env } = process;
 export const ARTSDATA: { ENDPOINT: string } = {
   ENDPOINT: env.ARTSDATA_ENDPOINT || "https://db.artsdata.ca/"
 };
+
+export const APPLICATION = {
+  HTTP_PORT: Number(env.APP_PORT) || 3000,
+  HTTPS_PORT: Number(env.APP_PORT) || 443
+};
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ExpressAdapter } from "@nestjs/platform-express";
 import { AppModule } from "./app.module";
+import { APPLICATION } from "./config/system.config";
+import * as express from "express";
+import * as fs from "fs";
+import * as http from "http";
+import * as https from "https";
+
+
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync("./secrets/private-key.pem"),
+    cert: fs.readFileSync("./secrets/public-certificate.pem")
+  };
+
+  const server = express();
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(server)
+  );
 
   const config = new DocumentBuilder()
     .setTitle("Artsdata.ca Reconciliation Service")
@@ -13,7 +30,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
-  await app.listen(3000);
+  await app.init();
+  // http.createServer(server).listen(APPLICATION.HTTP_PORT);
+  https.createServer(httpsOptions, server).listen(APPLICATION.HTTPS_PORT);
+
 }
 
 bootstrap();
