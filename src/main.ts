d4790791d93be9950aa06c2053844bd0a12386c3
sahalali <sahal@culturecreates.com>
2024-04-19 15:53:04 +0530

    Enabled cors

--- src/main.ts
+++ src/main.ts
@@ -30,6 +30,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api", app, document);
 
+  app.enableCors();
   await app.init();
   // http.createServer(server).listen(APPLICATION.HTTP_PORT);
   https.createServer(httpsOptions, server).listen(APPLICATION.HTTPS_PORT);
