d2b4b79ac2afcaec6053dcbb57f0dd15f4c97bcf
sahalali <sahal@culturecreates.com>
2024-04-29 16:48:06 +0530

    Updated query to support new lucene indexing

---
+++ src/config/graphdb-indexes.config.ts
@@ -0,0 +1,16 @@
+import * as dotenv from "dotenv";
+
+dotenv.config();
+const { env } = process;
+
+export const GRAPHDB_INDEX  = {
+  EVENT: env.EVENT || "event-index",
+  PLACE: env.PLACE || "place-index",
+  ORGANIZATION: env.ORGANIZATION || "organization-index",
+  PERSON: env.PERSON || "person-index",
+  CONCEPT: env.CONCEPT || "concept-index",
+  EVENT_TYPE: env.EVENT_TYPE || "event-type-index",
+  DEFAULT: env.DEFAULT || "resource-index",
+};
+
+
