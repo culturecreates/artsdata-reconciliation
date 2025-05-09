79a980f63fb60e646094be64ddd6e80002933b31
sahalali <sahal@culturecreates.com>
2025-05-09 15:33:50 +0400

    Added documentation to   MatchService

--- src/service/match/match.service.ts
+++ src/service/match/match.service.ts
@@ -14,6 +14,11 @@ export class MatchService {
               private readonly _manifestService: ManifestService) {
   }
 
+  /**
+   * @name reconcileByRawQueries
+   * @description Reconcile by raw queries
+   * @param rawQueries
+   */
   async reconcileByRawQueries(rawQueries: string): Promise<any> {
     if (!rawQueries) {
       return this._manifestService.getServiceManifest();
@@ -27,6 +32,13 @@ export class MatchService {
     return await this.reconcileByQueries(queries);
   }
 
+  /**
+   * @private
+   * @name reconcileByQueries
+   * @description Resolve property conditions
+   * @param rawSparqlQuery
+   * @param propertyConditions
+   */
   private _resolvePropertyConditions(rawSparqlQuery: string , propertyConditions: QueryCondition[]) {
     let propertyTriples: string = "";
     propertyConditions.forEach((condition , index) => {
@@ -37,6 +49,15 @@ export class MatchService {
 
   }
 
+  /**
+   * @private
+   * @name _getSparqlQuery
+   * @description Get SPARQL query
+   * @param name
+   * @param isQueryByURI
+   * @param type
+   * @param limit
+   */
   private _getSparqlQuery(name: string | undefined , isQueryByURI: boolean , type: string , limit: number | undefined): string {
     const graphdbIndex: string = ReconciliationServiceHelper.getGraphdbIndex(type);
 
@@ -60,6 +81,12 @@ export class MatchService {
       .replace("LIMIT_PLACE_HOLDER" , limit ? `LIMIT ${limit}` : "");
   }
 
+  /**
+   * @private
+   * @name _resolveConditions
+   * @description Resolve conditions
+   * @param conditions
+   */
   private _resolveConditions(conditions: QueryCondition[]) {
     const name = conditions
       .find(condition => condition.matchType == MatchTypeEnum.NAME)?.v;
@@ -68,6 +95,13 @@ export class MatchService {
     return { name , propertyConditions };
   }
 
+  /**
+   * @private
+   * @name _resolvePropertyValue
+   * @description Resolve property value
+   * @param value
+   * @param property
+   */
   private _resolvePropertyValue(value: string , property: string) {
     switch (property) {
       case ArtsdataProperties.START_DATE:
@@ -89,13 +123,19 @@ export class MatchService {
     }
   }
 
+  /**
+   * @private
+   * @name _generateTripleFromCondition
+   * @description Generate triple from condition
+   * @param condition
+   * @param index
+   */
   private _generateTripleFromCondition(condition: QueryCondition , index: number): string {
     const { required , pid , v: rawConditionValue , matchQualifier , matchQuantifier } = condition;
     const formattedConditionValue = this._resolvePropertyValue(rawConditionValue , pid as string);
     const formattedPropertyId = ReconciliationServiceHelper.isValidURI(pid as string) ? `<${pid}>` : `${pid}`;
 
-    let triple = "";
-    triple = this._resolveMatchQualifier(matchQualifier as MatchQualifierEnum , formattedPropertyId ,
+    let triple = this._resolveMatchQualifier(matchQualifier as MatchQualifierEnum , formattedPropertyId ,
       formattedConditionValue , index);
     triple = this._resolveMatchQuantifier(matchQuantifier as MatchQuantifierEnum , triple);
     triple = this._resolveRequired(triple , required as boolean);
@@ -103,6 +143,11 @@ export class MatchService {
     return triple;
   }
 
+  /**
+   * @name reconcileByQueries
+   * @description Reconcile by queries
+   * @param reconciliationRequest
+   */
   async reconcileByQueries(reconciliationRequest: ReconciliationRequest) {
 
     const { queries } = reconciliationRequest;
@@ -124,27 +169,51 @@ export class MatchService {
     return { results };
   }
 
+  /**
+   * @private
+   * @name _resolveMatchQualifier
+   * @description Resolve match qualifier, matchQualifier is defaulted to exact match
+   * @param matchQualifier
+   * @param formattedPropertyId
+   * @param formattedConditionValue
+   * @param index
+   */
   private _resolveMatchQualifier(matchQualifier: MatchQualifierEnum , formattedPropertyId: string ,
                                  formattedConditionValue: string , index: number) {
     if (!matchQualifier) {
       matchQualifier = MatchQualifierEnum.EXACT_MATCH;
     }
-    if (matchQualifier === MatchQualifierEnum.EXACT_MATCH) {
-      return `?entity ${formattedPropertyId} ${formattedConditionValue} .`;
-    } else if (matchQualifier == MatchQualifierEnum.REGEX_MATCH) {
-      const objectId = `?obj_${index + 1}`;
-      return `?entity ${formattedPropertyId} ${objectId}
-        FILTER REGEX(${objectId}, ${formattedConditionValue}, "i").`;
-    } else {
-      Exception.badRequest("Unsupported match qualifier");
+    switch (matchQualifier) {
+      case MatchQualifierEnum.EXACT_MATCH:
+        return `?entity ${formattedPropertyId} ${formattedConditionValue} .`;
+      case MatchQualifierEnum.REGEX_MATCH:
+        const objectId = `?obj_${index + 1}`;
+        return `?entity ${formattedPropertyId} ${objectId}
+          FILTER REGEX(${objectId}, ${formattedConditionValue}, "i").`;
+      default:
+        Exception.badRequest("Unsupported match qualifier");
+        return "";
     }
-    return "";
   }
 
+  /**
+   * @private
+   * @name _resolveRequired
+   * @description  Resolve required, required is defaulted to true
+   * @param triple
+   * @param required
+   */
   private _resolveRequired(triple: string , required: boolean) {
     return required ? triple : `OPTIONAL { ${triple} }`;
   }
 
+  /**
+   * @private
+   * @name _resolveMatchQuantifier
+   * @description Resolve match quantifier, matchQuantifier is defaulted to ALL
+   * @param matchQuantifier
+   * @param triple
+   */
   private _resolveMatchQuantifier(matchQuantifier: MatchQuantifierEnum , triple: string) {
     if (!matchQuantifier) {
       matchQuantifier = MatchQuantifierEnum.ALL;
