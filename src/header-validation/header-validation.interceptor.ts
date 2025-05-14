300e2e927932bb569be0e2743328e644a0fde8bb
sahalali <sahal@culturecreates.com>
2025-05-14 13:30:53 +0530

    Temporarily disables header

--- src/header-validation/header-validation.interceptor.ts
+++ src/header-validation/header-validation.interceptor.ts
@@ -6,15 +6,15 @@ export class HeaderValidationInterceptor implements NestInterceptor {
   constructor(private readonly expectedHeaderName: string, private readonly expectedHeaderValues: string[]) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const request = context.switchToHttp().getRequest();
-    const headerValue = request.headers[this.expectedHeaderName.toLowerCase()]; // Headers are case-insensitive
-
-    if (!headerValue || !this.expectedHeaderValues.includes(headerValue)) {
-      throw new HttpException(
-        `Invalid or missing header: '${this.expectedHeaderName}'. Expected one of: ${this.expectedHeaderValues.join(', ')}`,
-        HttpStatus.BAD_REQUEST,
-      );
-    }
+    // const request = context.switchToHttp().getRequest();
+    // const headerValue = request.headers[this.expectedHeaderName.toLowerCase()]; // Headers are case-insensitive
+    //
+    // if (!headerValue || !this.expectedHeaderValues.includes(headerValue)) {
+    //   throw new HttpException(
+    //     `Invalid or missing header: '${this.expectedHeaderName}'. Expected one of: ${this.expectedHeaderValues.join(', ')}`,
+    //     HttpStatus.BAD_REQUEST,
+    //   );
+    // }
 
     return next.handle();
   }
