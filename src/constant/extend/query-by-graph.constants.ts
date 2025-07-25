e7d9b8cbcc5112bcf5c74b9bc63ab86dca4f6f57
sahalali <sahal@culturecreates.com>
2025-07-25 23:56:03 +0530

    Updated sparql query for data extension by graph uri

--- src/constant/extend/query-by-graph.constants.ts
+++ src/constant/extend/query-by-graph.constants.ts
@@ -3,42 +3,44 @@ export const QUERY_BY_GRAPH = {
 PREFIX sh: <http://www.w3.org/ns/shacl#>
 PREFIX dbo: <http://dbpedia.org/ontology/>
 PREFIX wdt: <http://www.wikidata.org/prop/direct/>
-SELECT ?uri ?url
+SELECT ?uri 
+(sample(?urls) as ?url)
 (sample(?name_language) as ?name)  
-(sample(?isnis) as ?isni_uri)
-(sample(?adids) as ?artsdata_uri)
+(sample(?isni_uris) as ?isni_uri)
+(sample(?artsdata_uris) as ?artsdata_uri)
 (sample (?wikidata_ids) as ?wikidata_uri)
- (GROUP_CONCAT(?types ; SEPARATOR = ", ") AS ?type)
+(GROUP_CONCAT(?types ; SEPARATOR = ", ") AS ?type)
 WHERE {
     graph <GRAPH_URI_PLACEHOLDER> {
         ?uri schema:name ?name_language;
                  a ?types  .
     }
+    
     ?uri a schema:TYPE_PLACEHOLDER . 
     OPTIONAL {
       ?adids schema:sameAs ?uri .
       FILTER(contains(str(?adids),"http://kg.artsdata.ca/resource/K"))
     }
     
-    OPTIONAL {?uri schema:url ?url}
-  
     OPTIONAL {
       ?uri schema:sameAs ?adid_obj .
       FILTER(contains(str(?adid_obj),"http://kg.artsdata.ca/resource/K"))
     }
-    
+    BIND(COALESCE(?adid_obj, ?adid_sub) as ?artsdata_uris)
+
+    OPTIONAL {?uri schema:url ?urls}
+
     OPTIONAL {
       ?uri schema:sameAs ?wikidata_ids .
       FILTER(contains(str(?wikidata_ids),"http://www.wikidata.org/entity/"))
     }
     
     OPTIONAL {
-      ?uri schema:sameAs ?isnis .
-      FILTER(contains(str(?isnis),"https://isni.org/isni/"))
+      ?uri schema:sameAs ?isni_uris .
+      FILTER(contains(str(?isni_uris),"https://isni.org/isni/"))
     }
     
-    BIND(COALESCE(?adid_obj, ?adid_sub) as ?adid)
-} GROUP BY ?uri ?url
+} GROUP BY ?uri
 ORDER BY ?uri 
 LIMIT LIMIT_PLACEHOLDER
 OFFSET OFFSET_PLACEHOLDER` ,
