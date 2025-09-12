1502e9d5e8963b65f6464e9854c17fa9ba007e93
sahalali <sahal@culturecreates.com>
2025-09-12 16:18:14 +0530

    Updated propose extend API response

--- src/constant/extend/proposed-extend-properties.constants.ts
+++ src/constant/extend/proposed-extend-properties.constants.ts
@@ -1,108 +1,57 @@
 export const PROPOSED_EXTEND_PROPERTIES_METADATA = {
   EVENT: {
-    "type": "Event" ,
-    "properties": [
-      {
-        "id": "name" ,
-        "name": "name"
-      } ,
-      {
-        "id": "startDate" ,
-        "name": "startDate"
-      } ,
-      {
-        "id": "url" ,
-        "name": "url"
-      } ,
-      {
-        "id": "sameAs" ,
-        "name": "sameAs"
-      } ,
-      {
-        "id": "disambiguatingDescription" ,
-        "name": "disambiguatingDescription"
-      }]
+    type: "Event" ,
+    properties: [
+      { id: "name" , name: "name" } ,
+      { id: "startDate" , name: "startDate" } ,
+      { id: "endDate" , name: "endDate" } ,
+      { id: "disambiguatingDescription" , name: "disambiguatingDescription" } ,
+      { id: "additionalType" , name: "additionalType" } ,
+      { id: "url" , name: "url" } ,
+      { id: "sameAs" , name: "sameAs" } ,
+      { id: "eventStatus" , name: "eventStatus" } ,
+      { id: "eventAttendanceMode" , name: "eventAttendanceMode" } ,
+      { id: "location" , name: "location" } ,
+      { id: "offers" , name: "offers" , expandable: true } ,
+      { id: "performer" , name: "performer" , expandable: true } ,
+      { id: "organizer" , name: "organizer" , expandable: true }
+    ]
   } ,
   PLACE: {
-    "type": "Place" ,
-    "properties": [
-      {
-        "id": "name" ,
-        "name": "name"
-      } ,
-      {
-        "id": "url" ,
-        "name": "url"
-      } ,
-      {
-        "id": "sameAs" ,
-        "name": "sameAs"
-      } ,
-      {
-        "id": "disambiguatingDescription" ,
-        "name": "disambiguatingDescription"
-      } ,
-      {
-        "id": "address" ,
-        "name": "address"
-      }
+    type: "Place" ,
+    properties: [
+      { id: "name" , name: "name" } ,
+      { id: "url" , name: "url" } ,
+      { id: "sameAs" , name: "sameAs" } ,
+      { id: "disambiguatingDescription" , name: "disambiguatingDescription" } ,
+      { id: "address" , name: "address" , expandable: true }
     ]
-  } , PERSON: {
-    "type": "Person" ,
-    "properties": [
-      {
-        "id": "name" ,
-        "name": "name"
-      } ,
-      {
-        "id": "url" ,
-        "name": "url"
-      } ,
-      {
-        "id": "sameAs" ,
-        "name": "sameAs"
-      } ,
-      {
-        "id": "disambiguatingDescription" ,
-        "name": "disambiguatingDescription"
-      }]
-  } , ORGANIZATION: {
-    "type": "Organization" ,
-    "properties": [
-      {
-        "id": "name" ,
-        "name": "name"
-      } ,
-      {
-        "id": "url" ,
-        "name": "url"
-      } ,
-      {
-        "id": "sameAs" ,
-        "name": "sameAs"
-      } ,
-      {
-        "id": "disambiguatingDescription" ,
-        "name": "disambiguatingDescription"
-      }]
-  } , AGENT: {
-    "type": "Agent" ,
-    "properties": [
-      {
-        "id": "name" ,
-        "name": "name"
-      } ,
-      {
-        "id": "url" ,
-        "name": "url"
-      } ,
-      {
-        "id": "sameAs" ,
-        "name": "sameAs"
-      } ,
-      {
-        "id": "disambiguatingDescription" ,
-        "name": "disambiguatingDescription"
-      }]
+  } ,
+  PERSON: {
+    type: "Person" ,
+    properties: [
+      { id: "name" , name: "name" } ,
+      { id: "url" , name: "url" } ,
+      { id: "sameAs" , name: "sameAs" } ,
+      { id: "disambiguatingDescription" , name: "disambiguatingDescription" }
+    ]
+  } ,
+  ORGANIZATION: {
+    type: "Organization" ,
+    properties: [
+      { id: "name" , name: "name" } ,
+      { id: "url" , name: "url" } ,
+      { id: "sameAs" , name: "sameAs" } ,
+      { id: "disambiguatingDescription" , name: "disambiguatingDescription" }
+    ]
+  } ,
+  AGENT: {
+    type: "Agent" ,
+    properties: [
+      { id: "name" , name: "name" } ,
+      { id: "url" , name: "url" } ,
+      { id: "sameAs" , name: "sameAs" } ,
+      { id: "disambiguatingDescription" , name: "disambiguatingDescription" }
+    ]
   }
 };
