b475a65f4e8825cf04b0d096505eb4611ee9addb
sahalali <sahal@culturecreates.com>
2025-09-12 16:40:34 +0530

    cleanup

--- src/service/artsdata/artsdata.service.ts
+++ src/service/artsdata/artsdata.service.ts
@@ -1,45 +1,37 @@
 import { Injectable } from "@nestjs/common";
 import { ARTSDATA } from "../../config";
 import { HttpService } from "../http";
-import { Exception , MatchServiceHelper } from "../../helper";
-import { LanguageEnum } from "../../enum";
-import { QUERIES } from "../../constant";
-import { ResultCandidates } from "../../dto";
+import { Exception } from "../../helper";
 
 @Injectable()
 export class ArtsdataService {
-
   constructor(private readonly httpService: HttpService) {
   }
 
   /**
-   * @name _getArtsdataEndPoint
-   * @description Get the Artsdata endpoint
-   * @returns {string}
+   * Constructs the Artsdata SPARQL endpoint URL.
+   * @private
    */
   private _getArtsdataEndPoint(): string {
-    const route = "repositories/" + ARTSDATA.REPOSITORY;
-    const sparqlEndpoint = new URL(route , ARTSDATA.ENDPOINT);
-    return sparqlEndpoint.toString();
+    const route = `repositories/${ARTSDATA.REPOSITORY}`;
+    return new URL(route , ARTSDATA.ENDPOINT).toString();
   }
 
   /**
-   * @name executeSparqlQuery
-   * @description Get raw result for sparql query from Artsdata
+   * Executes a SPARQL query against the Artsdata endpoint.
    * @param sparqlQuery
    * @param infer
    */
-  async executeSparqlQuery(sparqlQuery: string , infer?: boolean): Promise<any> {
-    const sparqlEndpoint: string = this._getArtsdataEndPoint();
-    console.log(`Executing Sparql query:\n ${sparqlQuery}`);
-    const queryParam = `query=${encodeURIComponent(sparqlQuery)}&infer=${infer ? "true" : "false"}`;
+  async executeSparqlQuery(sparqlQuery: string , infer = false): Promise<any> {
+    const sparqlEndpoint = this._getArtsdataEndPoint();
+    console.log(`Executing Sparql query:\n${sparqlQuery}`);
+    const queryParam = `query=${encodeURIComponent(sparqlQuery)}&infer=${infer}`;
 
     try {
       return await this.httpService.postRequest(sparqlEndpoint , queryParam);
     } catch (error) {
       console.error("Error executing SPARQL query:" , error);
-      throw Exception.internalServerError("Error executing SPARQL query" + error.message);
+      throw Exception.internalServerError(`Error executing SPARQL query: ${error.message}`);
     }
   }
-
 }
