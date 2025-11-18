9da194d628eb88a27fafeaed191721e1ac16ed6d
sahal ali <sahal@culturecreates.com>
2025-11-18 11:18:57 +0400

    Reverting the change to the IDs in the response has modified the URIs.

--- src/helper/match-service.helper.ts
+++ src/helper/match-service.helper.ts
@@ -32,7 +32,7 @@ export class MatchServiceHelper {
             const currentBinding = currentBindings[0];
             const resultCandidate = new ResultCandidates();
 
-            resultCandidate.id = currentBinding["entity"].value;
+            resultCandidate.id = currentBinding["entity"].value?.split(ArtsdataConstants.PREFIX).pop();
             const name = currentBinding["name"]?.value;
             const nameEn = currentBinding["nameEn"]?.value;
             const nameFr = currentBinding["nameFr"]?.value;
