9735fdfc8ff6384cbc14788ba4069911d2538460
suhail <suhail@culturecreates.com>
2026-02-04 12:37:05 +0400

    Renamed APP_PORT to PORT system.config.ts, to fix heroku H10 issue.

--- src/config/system.config.ts
+++ src/config/system.config.ts
@@ -16,8 +16,8 @@ export const ARTSDATA: {
 };
 
 export const APPLICATION = {
-    HTTP_PORT: Number(env.APP_PORT) || 3000,
-    HTTPS_PORT: Number(env.APP_PORT) || 443,
+    HTTP_PORT: Number(env.PORT) || 3000,
+    HTTPS_PORT: Number(env.PORT) || 443,
     KG_URL: `https://${env.ENVIRONMENT === "production" ? "" : "staging."}kg.artsdata.ca/resource`,
     RECON_URL: `https://${env.ENVIRONMENT === "production" ? "" : "staging."}recon.artsdata.ca`
 };
