5a0b56975ce3b37db7f42bfa2d5eac0c67d78352
suhail <suhail@culturecreates.com>
2026-04-28 16:41:50 +0400

    Updated jest config

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,3 @@
-const shouldRunGlobal = process.env.ENVIRONMENT === 'development';
-
 module.exports = {
     moduleFileExtensions: ['js', 'json', 'ts'],
     rootDir: 'src',
@@ -9,9 +7,6 @@ module.exports = {
     },
     collectCoverageFrom: ['**/*.(t|j)s'],
     coverageDirectory: '../coverage',
-    testEnvironment: 'node',
+    testEnvironment: 'node'
 
-    // These will now ONLY be defined if you run 'npm run test:dev'
-    globalSetup: shouldRunGlobal ? '../test/setup.ts' : undefined,
-    globalTeardown: shouldRunGlobal ? '../test/global-teardown.ts' : undefined,
 };
