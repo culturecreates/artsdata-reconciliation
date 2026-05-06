ca0e1fbf646ae077876d4d1b9b546d0504bc0127
suhail <suhail@culturecreates.com>
2026-05-06 15:09:56 +0400

    Updated place entity reconciliation tests with new approach

--- src/service/match/match-places.service.spec.ts
+++ src/service/match/match-places.service.spec.ts
@@ -2,143 +2,174 @@ import {MatchService,} from "../../service";
 import {ReconciliationQuery} from "../../dto";
 import {Entities} from "../../constant";
 import {LanguageEnum, MatchTypeEnum} from "../../enum";
-import {executeAndCompareResults, setupMatchService} from "../../../test/util/common-util";
+import {
+    dropIndexAndTheGraph,
+    executeAndCompareResults,
+    setupMatchService,
+    uploadDataSetAndCreateLuceneConnector
+} from "../../../test/util/common-util";
+import {IndexFileNameEnum} from "../../enum/index-names.enum";
+import {MatchServiceHelper} from "../../helper";
+import {SparqlVersionEnum} from "../../enum/sparql-versions.enum";
 
 
-describe('Test reconciling places using sparql query version 2', () => {
+describe('Test reconciling Place using sparql query version 1', () => {
 
     let matchService: MatchService;
+    const testDatasetPath = 'test/fixtures/files/places-people-and-organizations-with-name.ttl';
+    let testLuceneConnectorId: string;
+    let testGraphUri: string;
 
     beforeAll(async () => {
         const setup = await setupMatchService();
         matchService = setup.matchService;
-    });
 
-    it('Reconcile a place with name `Roy Thomson Hall`, which is exact match', async () => {
+        const {
+            graphUri,
+            luceneConnector
+        } = await uploadDataSetAndCreateLuceneConnector(IndexFileNameEnum.PLACE, testDatasetPath)
+        testGraphUri = graphUri;
+        testLuceneConnectorId = luceneConnector;
+        jest.spyOn(MatchServiceHelper, 'getGraphdbIndex').mockReturnValue(luceneConnector);
+    });
+    afterAll(async () => {
+        await dropIndexAndTheGraph(testGraphUri, testLuceneConnectorId);
+    })
+
+    it('Reconcile a Place with name `Place bell`, which a exact match of name', async () => {
 
         const reconciliationQuery: ReconciliationQuery = {
             type: Entities.PLACE,
-            conditions: [{matchType: MatchTypeEnum.NAME, propertyValue: "Roy Thomson Hall"}],
+            conditions: [{matchType: MatchTypeEnum.NAME, propertyValue: "Place bell"}],
             limit: 1
         };
 
-        const expectedResult = {
-            id: "K11-19",
-            name: "Roy Thomson Hall",
-            type: Entities.PLACE,
-            match: false,
-            count: 1
-        }
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]}, SparqlVersionEnum.V1);
 
-        await executeAndCompareResults(matchService, expectedResult, reconciliationQuery);
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KP-1");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeFalsy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
+            .toBe("http://schema.org/Place");
+
     });
 
-    it(`Reconcile a place entity with name 'Roy Thomson', which is a close match`, async () => {
+    it('Reconcile a Place with name `Place`, which a close match of name', async () => {
 
         const reconciliationQuery: ReconciliationQuery = {
             type: Entities.PLACE,
-            conditions: [{matchType: MatchTypeEnum.NAME, propertyValue: "Roy Thomson"}],
+            conditions: [{matchType: MatchTypeEnum.NAME, propertyValue: "Place"}],
             limit: 1
         };
 
-        const expectedResult = {
-            id: "K11-19",
-            name: "Roy Thomson Hall",
-            type: Entities.PLACE,
-            match: false,
-            count: 1
-        }
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]}, SparqlVersionEnum.V1);
 
-        await executeAndCompareResults(matchService, expectedResult, reconciliationQuery);
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KP-1");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeFalsy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
+            .toBe("http://schema.org/Place");
+
     });
 
-    it(`Reconcile a place entity with name 'Thomson Hall', which is a close match`, async () => {
+    it(`Reconcile an place entity with uri 'http://kg.artsdata.ca/resource/KP-1`, async () => {
 
         const reconciliationQuery: ReconciliationQuery = {
             type: Entities.PLACE,
-            conditions: [{matchType: MatchTypeEnum.NAME, propertyValue: "Thomson Hall"}],
+            conditions: [{matchType: MatchTypeEnum.ID, propertyValue: "http://kg.artsdata.ca/resource/KP-1"}],
             limit: 1
         };
 
-        const expectedResult = {
-            id: "K11-19",
-            name: "Roy Thomson Hall",
-            type: Entities.PLACE,
-            match: false,
-            count: 1
-        }
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]}, SparqlVersionEnum.V1);
 
-        await executeAndCompareResults(matchService, expectedResult, reconciliationQuery);
-    });
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
 
-    it(`Reconcile a place entity with accented name 'Amphithéâtre Cogeco', which is close match`, async () => {
-
-        const reconciliationQuery: ReconciliationQuery = {
-            type: Entities.PLACE,
-            conditions: [{matchType: MatchTypeEnum.NAME, propertyValue: "Amphithéâtre Cogeco"}],
-            limit: 1
-        };
-
-        const expectedResult = {
-            id: "K5-463",
-            name: "Amphithéâtre Cogeco",
-            type: Entities.PLACE,
-            match: false,
-            count: 1
-        }
-
-        await executeAndCompareResults(matchService, expectedResult, reconciliationQuery);
-    });
-
-    it(`Reconcile an place entity with uri 'http://kg.artsdata.ca/resource/K11-192`, async () => {
-
-        const reconciliationQuery: ReconciliationQuery = {
-            type: Entities.PLACE,
-            conditions: [{matchType: MatchTypeEnum.ID, propertyValue: "http://kg.artsdata.ca/resource/K11-192"}],
-            limit: 1
-        };
-
-        const expectedResult = {
-            id: "K11-192",
-            name: "Sanderson Centre",
-            type: Entities.PLACE,
-            match: false,
-            count: 1
-        }
-
-        await executeAndCompareResults(matchService, expectedResult, reconciliationQuery);
-    });
-
-    it(`Reconcile National Arts Centre - Azrieli Studio using English UI`, async () => {
-        const reconciliationQuery: ReconciliationQuery = {
-            type: Entities.PLACE,
-            conditions: [{
-                matchType: MatchTypeEnum.NAME,
-                propertyValue: "National Arts Centre - Azrieli Studio"
-            }],
-            limit: 1
-        };
-
-        const result = await matchService.reconcileByQueries(LanguageEnum.ENGLISH, {queries: [reconciliationQuery]}, "v2");
-        const candidate = result.results?.[0]?.candidates?.[0];
-        expect(candidate.name).toBe("National Arts Centre - Azrieli Studio");
-        expect(candidate.id).toBe("K11-15");
-    });
-
-    it(`Reconcile National Arts Centre - Azrieli Studio using French UI`, async () => {
-        const reconciliationQuery: ReconciliationQuery = {
-            type: Entities.PLACE,
-            conditions: [{
-                matchType: MatchTypeEnum.NAME,
-                propertyValue: "National Arts Centre - Azrieli Studio"
-            }],
-            limit: 1
-        };
-
-        const result = await matchService.reconcileByQueries(LanguageEnum.FRENCH, {queries: [reconciliationQuery]}, "v2");
-        const candidate = result.results?.[0]?.candidates?.[0];
-        expect(candidate.name).toBe("Centre national des arts - Studio Azrieli");
-        expect(candidate.id).toBe("K11-15");
+        expect(actualResult?.id).toBe("KP-1");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeTruthy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
+            .toBe("http://schema.org/Place");
     });
 });
 
+describe('Test reconciling place using sparql query version 2', () => {
+
+    let matchService: MatchService;
+    const testDatasetPath = 'test/fixtures/files/places-people-and-organizations-with-name.ttl';
+    let testLuceneConnectorId: string;
+    let testGraphUri: string;
+
+    beforeAll(async () => {
+        const setup = await setupMatchService();
+        matchService = setup.matchService;
+
+        const {
+            graphUri,
+            luceneConnector
+        } = await uploadDataSetAndCreateLuceneConnector(IndexFileNameEnum.ALL_LITERALS, testDatasetPath)
+        testGraphUri = graphUri;
+        testLuceneConnectorId = luceneConnector;
+        jest.spyOn(MatchServiceHelper, 'getGraphdbIndex').mockReturnValue(luceneConnector);
+    });
+    afterAll(async () => {
+        await dropIndexAndTheGraph(testGraphUri, testLuceneConnectorId);
+    })
+
+    it('Reconcile an place with name `Place bell`, which is exact match', async () => {
+
+        const reconciliationQuery: ReconciliationQuery = {
+            type: Entities.PLACE,
+            conditions: [{matchType: MatchTypeEnum.NAME, propertyValue: "Place bell"}],
+            limit: 1
+        };
+
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]}, SparqlVersionEnum.V2);
+
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KP-1");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeFalsy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
+            .toBe("http://schema.org/Place");
+
+    });
+
+    it(`Reconcile an Place entity with uri 'http://kg.artsdata.ca/resource/KP-1`, async () => {
+
+        const reconciliationQuery: ReconciliationQuery = {
+            type: Entities.PLACE,
+            conditions: [{matchType: MatchTypeEnum.ID, propertyValue: "http://kg.artsdata.ca/resource/KP-1"}],
+            limit: 1
+        };
+
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]}, SparqlVersionEnum.V2);
+
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KP-1");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeTruthy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
+            .toBe("http://schema.org/Place");
+    });
+});
