b3b79469bb1d6291794f7046ab8caf2a5173db2a
suhail <suhail@culturecreates.com>
2026-05-06 12:21:32 +0400

    Deleted old test and renamed new test

--- src/service/match/match-events.service.spec.ts
+++ src/service/match/match-events.service.spec.ts
@@ -1,55 +1,152 @@
 import {MatchService,} from "../../service";
 import {ReconciliationQuery} from "../../dto";
 import {Entities} from "../../constant";
-import {MatchTypeEnum} from "../../enum";
-import {executeAndCompareResults, setupMatchService} from "../../../test/util/common-util";
+import {LanguageEnum, MatchTypeEnum} from "../../enum";
+import {
+    dropIndexAndTheGraph,
+    setupMatchService,
+    uploadDataSetAndCreateLuceneConnector
+} from "../../../test/util/common-util";
+import {IndexFileNameEnum} from "../../enum/index-names.enum";
+import {MatchServiceHelper} from "../../helper";
+import {SparqlVersionEnum} from "../../enum/sparql-versions.enum";
 
 
-describe('Test reconciling events using sparql query version 2', () => {
+describe('Test reconciling events using sparql query version 1', () => {
 
     let matchService: MatchService;
+    const testDatasetPath = 'test/fixtures/files/events-with-name.ttl';
+    let testLuceneConnectorId: string;
+    let testGraphUri: string;
 
     beforeAll(async () => {
         const setup = await setupMatchService();
         matchService = setup.matchService;
-    });
 
-    it('Reconcile an event with name `Émile Bilodeau`, which is exact match', async () => {
+        const {
+            graphUri,
+            luceneConnector
+        } = await uploadDataSetAndCreateLuceneConnector(IndexFileNameEnum.EVENT, testDatasetPath)
+        testGraphUri = graphUri;
+        testLuceneConnectorId = luceneConnector;
+        jest.spyOn(MatchServiceHelper, 'getGraphdbIndex').mockReturnValue(luceneConnector);
+    });
+    afterAll(async () => {
+        await dropIndexAndTheGraph(testGraphUri,testLuceneConnectorId);
+    })
+
+    it('Reconcile an event with name `A Beacon in the Night`, which is exact match', async () => {
 
         const reconciliationQuery: ReconciliationQuery = {
             type: Entities.EVENT,
-            conditions: [{matchType: MatchTypeEnum.NAME, propertyValue: "Émile Bilodeau"}],
+            conditions: [{matchType: MatchTypeEnum.NAME, propertyValue: "A Beacon in the Night"}],
             limit: 1
         };
 
-        const expectedResult = {
-            id: "K23-5524",
-            name: "Émile Bilodeau",
-            type: Entities.EVENT,
-            match: false,
-            count: 1
-        }
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]});
 
-        await executeAndCompareResults(matchService, expectedResult, reconciliationQuery);
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KE-1");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeFalsy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Event")?.id)
+            .toBe("http://schema.org/Event");
+
     });
 
-    it(`Reconcile an event entity with uri 'http://kg.artsdata.ca/resource/K23-5524`, async () => {
+    it(`Reconcile an event entity with uri 'http://kg.artsdata.ca/resource/KE-4`, async () => {
 
         const reconciliationQuery: ReconciliationQuery = {
             type: Entities.EVENT,
-            conditions: [{matchType: MatchTypeEnum.ID, propertyValue: "http://kg.artsdata.ca/resource/K23-5524"}],
+            conditions: [{matchType: MatchTypeEnum.ID, propertyValue: "http://kg.artsdata.ca/resource/KE-4"}],
             limit: 1
         };
 
-        const expectedResult = {
-            id: "K23-5524",
-            name: "Émile Bilodeau",
-            type: Entities.EVENT,
-            match: false,
-            count: 1
-        }
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]});
 
-        await executeAndCompareResults(matchService, expectedResult, reconciliationQuery);
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KE-4");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeTruthy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Event")?.id)
+            .toBe("http://schema.org/Event");
+    });
+});
+
+describe('Test reconciling events using sparql query version 2', () => {
+
+    let matchService: MatchService;
+    const testDatasetPath = 'test/fixtures/files/events-with-name.ttl';
+    let testLuceneConnectorId: string;
+    let testGraphUri: string;
+
+    beforeAll(async () => {
+        const setup = await setupMatchService();
+        matchService = setup.matchService;
+
+        const {
+            graphUri,
+            luceneConnector
+        } = await uploadDataSetAndCreateLuceneConnector(IndexFileNameEnum.ALL_LITERALS, testDatasetPath)
+        testGraphUri = graphUri;
+        testLuceneConnectorId = luceneConnector;
+        jest.spyOn(MatchServiceHelper, 'getGraphdbIndex').mockReturnValue(luceneConnector);
+    });
+    afterAll(async () => {
+        await dropIndexAndTheGraph(testGraphUri,testLuceneConnectorId);
+    })
+
+    it('Reconcile an event with name `A Beacon in the Night`, which is exact match', async () => {
+
+        const reconciliationQuery: ReconciliationQuery = {
+            type: Entities.EVENT,
+            conditions: [{matchType: MatchTypeEnum.NAME, propertyValue: "A Beacon in the Night"}],
+            limit: 1
+        };
+
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]},SparqlVersionEnum.V2);
+
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KE-1");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeFalsy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Event")?.id)
+            .toBe("http://schema.org/Event");
+
+    });
+
+    it(`Reconcile an event entity with uri 'http://kg.artsdata.ca/resource/KE-4`, async () => {
+
+        const reconciliationQuery: ReconciliationQuery = {
+            type: Entities.EVENT,
+            conditions: [{matchType: MatchTypeEnum.ID, propertyValue: "http://kg.artsdata.ca/resource/KE-4"}],
+            limit: 1
+        };
+
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]},"V2");
+
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KE-4");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeTruthy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Event")?.id)
+            .toBe("http://schema.org/Event");
     });
 });
 
