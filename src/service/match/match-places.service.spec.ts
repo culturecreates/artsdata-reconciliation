a25e43f0d855ca31efcf0946c9c5927f072c99c3
suhail <suhail@culturecreates.com>
2026-05-06 18:00:57 +0400

    Added reconciling places tests

--- src/service/match/match-places.service.spec.ts
+++ src/service/match/match-places.service.spec.ts
@@ -82,6 +82,34 @@ describe('Test reconciling Place using sparql query version 1', () => {
 
     });
 
+    it('It should match a place whose French title is the search term', async () => {
+
+        const reconciliationQuery: ReconciliationQuery = {
+            type: "schema:Place",
+            limit: 1,
+            conditions: [
+                {
+                    matchType: "name",
+                    propertyValue: "Place des arts"
+                }
+            ]
+        }
+
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]}, SparqlVersionEnum.V1);
+
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KP-3");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeFalsy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
+            .toBe("http://schema.org/Place");
+
+    });
+
     it(`Reconcile an place entity with uri 'http://kg.artsdata.ca/resource/KP-1`, async () => {
 
         const reconciliationQuery: ReconciliationQuery = {
@@ -103,6 +131,193 @@ describe('Test reconciling Place using sparql query version 1', () => {
         expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
             .toBe("http://schema.org/Place");
     });
+
+    it('It should match a place whose French title is the search term', async () => {
+
+        const reconciliationQuery: ReconciliationQuery = {
+            type: "schema:Place",
+            limit: 1,
+            conditions: [
+                {
+                    matchType: "name",
+                    propertyValue: "Place des arts"
+                }
+            ]
+        }
+
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]}, SparqlVersionEnum.V2);
+
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KP-3");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeFalsy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
+            .toBe("http://schema.org/Place");
+
+    });
+
+    it('Reconcile Place with Name and Postal code', async () => {
+
+        const reconciliationQuery: ReconciliationQuery = {
+            type: "schema:Place",
+            limit: 1,
+            conditions: [
+                {
+                    matchType: "name",
+                    propertyValue: "Place des arts",
+                },
+                {
+                    matchType: "property",
+                    propertyValue: "H2X 1Z8",
+                    propertyId: "schema:address/schema:postalCode",
+                    required: true,
+                },
+            ],
+        }
+
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]}, SparqlVersionEnum.V2);
+
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KP-3");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeFalsy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
+            .toBe("http://schema.org/Place");
+
+    });
+
+    it('Reconcile Place with Name and Street Address', async () => {
+
+        const reconciliationQuery: ReconciliationQuery ={
+            type: "schema:Place",
+            limit: 1,
+            conditions: [
+                {
+                    matchType: "name",
+                    propertyValue: "Place des arts",
+                },
+                {
+                    matchType: "property",
+                    propertyValue: "175, rue Sainte-Catherine Ouest",
+                    propertyId: "schema:address/schema:streetAddress",
+                    required: true,
+                },
+            ],
+        }
+
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]}, SparqlVersionEnum.V1);
+
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KP-3");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeFalsy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
+            .toBe("http://schema.org/Place");
+
+    });
+
+    it('It should find alternate names', async () => {
+
+        const reconciliationQuery: ReconciliationQuery = {
+            type: "schema:Place",
+            limit: 1,
+            conditions: [
+                {
+                    matchType: "name",
+                    propertyValue: "Place des arts de Montréal"
+                }
+            ]
+        }
+
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]}, SparqlVersionEnum.V1);
+
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KP-3");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeFalsy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
+            .toBe("http://schema.org/Place");
+
+    });
+
+    it('Reconcile Place with name and postal code and match should be false since postal code is incorrect', async () => {
+
+        const reconciliationQuery: ReconciliationQuery = {
+            type: "schema:Place",
+            limit: 1,
+            conditions: [
+                {
+                    matchType: "name",
+                    propertyValue: "Place des arts de Montréal"
+                }
+            ]
+        }
+
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]}, SparqlVersionEnum.V1);
+
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KP-3");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeFalsy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
+            .toBe("http://schema.org/Place");
+
+    });
+
+    it('Reconcile Place with name and locality, the match should be true since all are exactly matching', async () => {
+
+        const reconciliationQuery: ReconciliationQuery =  {
+            type: "schema:Place",
+            limit: 1,
+            conditions: [
+                {
+                    matchType: "name",
+                    propertyValue: "Place des Arts",
+                },
+                {
+                    matchType: "property",
+                    propertyValue: "Toronto",
+                    propertyId:
+                        "<http://schema.org/address>/<http://schema.org/addressLocality>",
+                },
+            ],
+        };
+
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]});
+
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KP-3");
+        expect(allResults?.length).toBe(1);
+        expect(actualResult?.match).toBeTruthy();
+        expect(actualResult?.type?.find(type => type.id === "http://schema.org/Place")?.id)
+            .toBe("http://schema.org/Place");
+
+    });
+
 });
 
 describe('Test reconciling place using sparql query version 2', () => {
@@ -150,6 +365,9 @@ describe('Test reconciling place using sparql query version 2', () => {
             .toBe("http://schema.org/Place");
 
     });
+
+
+
 
     it(`Reconcile an Place entity with uri 'http://kg.artsdata.ca/resource/KP-1`, async () => {
 
