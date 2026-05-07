4bc375d943554ca3e8872d57a30e209017960ac8
suhail <suhail@culturecreates.com>
2026-05-07 15:58:17 +0400

    Added tests with edit distance for fuzzy matching

---
+++ src/service/match/match-events-fuzzy-search.service.spec.ts
@@ -0,0 +1,58 @@
+import {MatchService,} from "../../service";
+import {ReconciliationQuery} from "../../dto";
+import {Entities} from "../../constant";
+import {LanguageEnum, MatchTypeEnum} from "../../enum";
+import {
+    dropIndexAndTheGraph,
+    setupMatchService,
+    uploadDataSetAndCreateLuceneConnector
+} from "../../../test/util/common-util";
+import {IndexFileNameEnum} from "../../enum/index-names.enum";
+import {MatchServiceHelper} from "../../helper";
+import {SparqlVersionEnum} from "../../enum/sparql-versions.enum";
+
+
+describe('Test reconciling events using sparql query version 1', () => {
+
+    let matchService: MatchService;
+    const testDatasetPath = 'test/fixtures/files/places-for-fuzzy-search.ttl';
+    let testLuceneConnectorId: string;
+    let testGraphUri: string;
+
+    beforeAll(async () => {
+        const setup = await setupMatchService();
+        matchService = setup.matchService;
+
+        const {
+            graphUri,
+            luceneConnector
+        } = await uploadDataSetAndCreateLuceneConnector(IndexFileNameEnum.PLACE, testDatasetPath)
+        testGraphUri = graphUri;
+        testLuceneConnectorId = luceneConnector;
+        jest.spyOn(MatchServiceHelper, 'getGraphdbIndex').mockReturnValue(luceneConnector);
+    });
+    afterAll(async () => {
+        await dropIndexAndTheGraph(testGraphUri,testLuceneConnectorId);
+    })
+
+    it('Reconcile an place with name `Theatre`, which is fuzzy match to Théâtre de la Cour des Arts', async () => {
+
+        const reconciliationQuery: ReconciliationQuery = {
+            type: Entities.PLACE,
+            conditions: [{matchType: MatchTypeEnum.NAME, propertyValue: "Theatre"}],
+            limit: 1
+        };
+
+        const response = await matchService.reconcileByQueries(LanguageEnum.ENGLISH,
+            {queries: [reconciliationQuery]});
+
+        expect(response.results).toHaveLength(1);
+        const allResults = response.results?.[0]?.candidates;
+        const actualResult = allResults?.[0];
+
+        expect(actualResult?.id).toBe("KP-4");
+
+    });
+
+});
+
