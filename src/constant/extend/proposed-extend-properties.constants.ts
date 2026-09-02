5033eae35006b1f89fe2b39c211c7c882c60bda1
suhail <suhail@culturecreates.com>
2026-09-02 15:48:46 +0400

    Modified extend proposed APi response to include rdf-type

--- src/constant/extend/proposed-extend-properties.constants.ts
+++ src/constant/extend/proposed-extend-properties.constants.ts
@@ -1,57 +1,64 @@
+import {RDF} from "../shared";
+
 export const PROPOSED_EXTEND_PROPERTIES_METADATA = {
-  EVENT: {
-    type: "Event" ,
-    properties: [
-      { id: "name" , name: "name" } ,
-      { id: "startDate" , name: "startDate" } ,
-      { id: "endDate" , name: "endDate" } ,
-      { id: "disambiguatingDescription" , name: "disambiguatingDescription" } ,
-      { id: "additionalType" , name: "additionalType" } ,
-      { id: "url" , name: "url" } ,
-      { id: "sameAs" , name: "sameAs" } ,
-      { id: "eventStatus" , name: "eventStatus" } ,
-      { id: "eventAttendanceMode" , name: "eventAttendanceMode" } ,
-      { id: "location" , name: "location" } ,
-      { id: "offers" , name: "offers" , expandable: true } ,
-      { id: "performer" , name: "performer" , expandable: true } ,
-      { id: "organizer" , name: "organizer" , expandable: true }
-    ]
-  } ,
-  PLACE: {
-    type: "Place" ,
-    properties: [
-      { id: "name" , name: "name" } ,
-      { id: "url" , name: "url" } ,
-      { id: "sameAs" , name: "sameAs" } ,
-      { id: "disambiguatingDescription" , name: "disambiguatingDescription" } ,
-      { id: "address" , name: "address" , expandable: true }
-    ]
-  } ,
-  PERSON: {
-    type: "Person" ,
-    properties: [
-      { id: "name" , name: "name" } ,
-      { id: "url" , name: "url" } ,
-      { id: "sameAs" , name: "sameAs" } ,
-      { id: "disambiguatingDescription" , name: "disambiguatingDescription" }
-    ]
-  } ,
-  ORGANIZATION: {
-    type: "Organization" ,
-    properties: [
-      { id: "name" , name: "name" } ,
-      { id: "url" , name: "url" } ,
-      { id: "sameAs" , name: "sameAs" } ,
-      { id: "disambiguatingDescription" , name: "disambiguatingDescription" }
-    ]
-  } ,
-  AGENT: {
-    type: "Agent" ,
-    properties: [
-      { id: "name" , name: "name" } ,
-      { id: "url" , name: "url" } ,
-      { id: "sameAs" , name: "sameAs" } ,
-      { id: "disambiguatingDescription" , name: "disambiguatingDescription" }
-    ]
-  }
+    EVENT: {
+        type: "Event",
+        properties: [
+            {id: RDF.TYPE, name: "type"},
+            {id: "name", name: "name"},
+            {id: "startDate", name: "startDate"},
+            {id: "endDate", name: "endDate"},
+            {id: "disambiguatingDescription", name: "disambiguatingDescription"},
+            {id: "additionalType", name: "additionalType"},
+            {id: "url", name: "url"},
+            {id: "sameAs", name: "sameAs"},
+            {id: "eventStatus", name: "eventStatus"},
+            {id: "eventAttendanceMode", name: "eventAttendanceMode"},
+            {id: "location", name: "location"},
+            {id: "offers", name: "offers", expandable: true},
+            {id: "performer", name: "performer", expandable: true},
+            {id: "organizer", name: "organizer", expandable: true}
+        ]
+    },
+    PLACE: {
+        type: "Place",
+        properties: [
+            {id: RDF.TYPE, name: "type"},
+            {id: "name", name: "name"},
+            {id: "url", name: "url"},
+            {id: "sameAs", name: "sameAs"},
+            {id: "disambiguatingDescription", name: "disambiguatingDescription"},
+            {id: "address", name: "address", expandable: true}
+        ]
+    },
+    PERSON: {
+        type: "Person",
+        properties: [
+            {id: RDF.TYPE, name: "type"},
+            {id: "name", name: "name"},
+            {id: "url", name: "url"},
+            {id: "sameAs", name: "sameAs"},
+            {id: "disambiguatingDescription", name: "disambiguatingDescription"}
+        ]
+    },
+    ORGANIZATION: {
+        type: "Organization",
+        properties: [
+            {id: RDF.TYPE, name: "type"},
+            {id: "name", name: "name"},
+            {id: "url", name: "url"},
+            {id: "sameAs", name: "sameAs"},
+            {id: "disambiguatingDescription", name: "disambiguatingDescription"}
+        ]
+    },
+    AGENT: {
+        type: "Agent",
+        properties: [
+            {id: RDF.TYPE, name: "type"},
+            {id: "name", name: "name"},
+            {id: "url", name: "url"},
+            {id: "sameAs", name: "sameAs"},
+            {id: "disambiguatingDescription", name: "disambiguatingDescription"}
+        ]
+    }
 };
